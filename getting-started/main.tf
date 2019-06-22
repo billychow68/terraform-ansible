1aeb67e431ad61d0268006d988e18be471be3d62
billychow68 <billchow07@gmail.com>
2019-06-22 12:02:01 -0700

    Create variable for SSH port

--- getting-started/main.tf
+++ getting-started/main.tf
@@ -70,15 +70,9 @@ resource "aws_eip" "eip2" {
 resource "aws_security_group" "training" {
   name        = "training"
   ingress {
-    from_port     = 22
-    to_port       = 22
+    from_port     = "${var.ssh_port}"
+    to_port       = "${var.ssh_port}"
     protocol      = "tcp"
-    cidr_blocks   = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port     = 0
-    to_port       = 0
-    protocol      = "-1"
     cidr_blocks   = ["0.0.0.0/0"]
   }
   tags = {
--- getting-started/variables.tf
+++ getting-started/variables.tf
@@ -10,3 +10,7 @@ variable "amis" {
     "us-west-2" = "ami-4b32be2b"
   }
 }
+
+variable "ssh_port" {
+  default = "22"
+}
