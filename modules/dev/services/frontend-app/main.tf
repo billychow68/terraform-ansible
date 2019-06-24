73932fa017a8253bcab157bccf142bc1afc68496
billychow68 <billchow07@gmail.com>
2019-06-24 12:41:47 -0700

    Update

--- modules/dev/services/frontend-app/main.tf
+++ modules/dev/services/frontend-app/main.tf
@@ -1,4 +1,6 @@
 
 module "frontend" {
-  source = "/modules/frontend-app"
+  source = "../../../modules/frontend-app"
+  min_size = 1
+  max_size = 2
 }
---
+++ modules/modules/frontend-app/vars.tf
@@ -0,0 +1,7 @@
+
+variable "min_size" {
+  description = "The minimum number of EC2 instances in the ASG"
+}
+variable "max_size" {
+  description = "The maximum number of EC2 instances in the ASG"
+}
