d15ca5d44f4b321f2c8dade81445792d972b71fc
billychow68 <billchow07@gmail.com>
2019-06-24 12:42:29 -0700

    Fixed vars

--- modules/modules/frontend-app/vars.tf
+++ modules/modules/frontend-app/vars.tf
@@ -5,3 +5,19 @@ variable "min_size" {
 variable "max_size" {
   description = "The maximum number of EC2 instances in the ASG"
 }
+variable "region" {
+  default = "us-east-1"
+}
+variable "amis" {
+  type = map
+  default = {
+    "us-east-1" = "ami-b374d5a5"
+    "us-west-2" = "ami-4b32be2b"
+  }
+}
+variable "ssh_port" {
+  default = "22"
+}
+variable "server_port" {
+  default = "8080"
+}
